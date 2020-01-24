d564949af0d8779b45b00a5a9811e17ca47c1705
Emilio Araos <ea3dev@gmail.com>
2020-01-23 18:04:34 -0700

    tuples basic

---
+++ tuples.py
@@ -0,0 +1,20 @@
+t = "a", "b", "C"   # This is a tuple
+print(t)
+
+print("a", "b", "c")   # Not a tuple
+print(("a", "b", "c"))    # This is also a tuple
+
+welcome = "Welcome to my nightmare", "Alice Copper", 1975
+
+bad = "Bad Company" "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda Day", 2011
+metallica = "Ride the lightning", "Metallica", 1984
+
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+
+metallica[0] = "Master of puppets"
