43fbee0615fa5830c0653ba3fd8a2bd703aa1e2b
Emilio Araos <ea3dev@gmail.com>
2020-01-24 18:28:35 -0700

    unpacking tuples

--- tuples.py
+++ tuples.py
@@ -8,11 +8,18 @@ welcome = "Welcome to my nightmare", "Alice Copper", 1975
 
 bad = "Bad Company" "Bad Company", 1974
 budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Imelda Day", 2011
-metallica = "Ride the lightning", "Metallica", 1984
+imelda = "More Mayhem", "Imelda Day", 2011, ((1,"Pulling the rug"), (2, "Phsyco"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
+metallica = ["Ride the lightning", "Metallica", 1984]
 
-print(metallica)
-title, artist, year = imelda
+print(imelda)
+
+title, artist, year, tracks = imelda
+
 print(title)
 print(artist)
 print(year)
+print(tracks)
+
+for song in tracks:
+    track, title = song
+    print(f"\tTrack number {track}, Title: {title}")
